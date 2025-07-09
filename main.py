5474e8b37c9f87950717372a448f91161c499724
Nicholas <nicholasbence12@gmail.com>
2025-07-09 14:57:24 +0200

    Version 0.2. Allowed player to place. Created 'show_board'. Introduced PC but needs to be smarter. Only need to determine winner now

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from tictactoe import TicTacToe
 import os
 
+
 # clears console depending on OS
 def clear_console():
-    os.system('cls' if os.name == 'nt' else 'clear')
+    os.system("cls" if os.name == "nt" else "clear")
 
 
 # recurring function
@@ -23,34 +24,38 @@ def running():
     # while bContinue:
     print("You are X:")
 
-# loops until no one can play anymore
+    # loops until no one can play anymore
     while not gameOver:
+        bPlayer_turn = True
         try:
-            row = int(input("Choose the row you want to place your x: "))
-            column = int(input("Choose the column you want to place your x: "))
-            if row-1 > 2 or row-1 < 0:
-                print("You need to choose between 1-3.") # 1 2 3 but in list format it is 0 1 2
-            elif column-1 > 2 or column-1 < 0:
-                print("You need to choose between 1-3.")
-            else:
-                game.player_move(row,column)
+
+            if bPlayer_turn:
+                row = int(input("Choose the row you want to place your x: ")) - 1
+                column = int(input("Choose the column you want to place your x: ")) - 1
+                if row > 2 or row < 0:
+                    print(
+                        "You need to choose between 1-3."
+                    )  # 1 2 3 but in list format it is 0 1 2
+                elif column > 2 or column < 0:
+                    print("You need to choose between 1-3.")
+                else:
+                    game.player_move(row, column)
+                    bPlayer_turn = False
+                    game.pc_move()
         except ValueError:
             print("Only insert numbers, silly!")
-    
+
     # TODO - Determine winner
 
     # TODO - Ask player if they want to continue
     qContinue = input("Do you want to play again? Type 'y' or 'n': ")
     # if yes, recursion, if no, quit program
-    if (qContinue == 'y'):
+    if qContinue == "y":
         clear_console()
         return running()
-    elif (qContinue == 'n'):
+    elif qContinue == "n":
         print("\nHave a good day.\n")
         # bContinue = False
 
-    
 
 running()
-       
-
--- tictactoe.py
+++ tictactoe.py
@@ -1,24 +1,72 @@
+import random
+import time
+
+
 class TicTacToe:
     def __init__(self):
-        self.board = [['-' for _ in range(3)] for _ in range(3)] # Empty board used for Tic Tac Toe
+        self.board = [
+            ["-" for _ in range(3)] for _ in range(3)
+        ]  # Empty board used for Tic Tac Toe
         self.gameOver = False
 
-# Hardcoded version:
-# board = [
-#     ['-', '-', '-'],
-#     ['-', '-', '-'],
-#     ['-', '-', '-']
-# ]
-    def player_move(self,row,column):
-        pass
+    # Hardcoded version:
+    # board = [
+    #     ['-', '-', '-'],
+    #     ['-', '-', '-'],
+    #     ['-', '-', '-']
+    # ]
+    def player_move(self, row, column):
+
+        if self.board[row][column] == "X" or self.board[row][column] == "O":
+            print("You cannot place your X there.")
+        elif self.board[row][column] == "-":
+            self.board[row][column] = "X"
+            self.show_board()
 
     def pc_move(self):
-        pass
+        print("PC is placing his circle...\n")
+        time.sleep(2)
+        while True:
+            x = random.randint(0, 2)
+            y = random.randint(0, 2)
+            if self.board[x][y] == "-":
+                self.board[x][y] = "O"
+                print(f"PC placed O at {chr(65 + x)}{y + 1}")
+                self.show_board()
+
+                break
 
     def determine_winner(self):
         pass
 
+        #    1   2   3
+        # A  X | O |
+        #   ---+---+---
+        # B    | X |
+        #   ---+---+---
+        # C    |   | O
+
+    def show_board(self):
+        # print("    1   2   3")
+        # alpha = ["A", "B", "C"]
+        # counter = 0
+        # for row in self.board:
+        #     printRow = f" {alpha[counter]} "
+        #     counter += 1
+        #     for column in row:
+        #         printRow += f" {column} |"
+        #     print(printRow)
+        #     print("   ---+---+---")
+
+        print("    1   2   3")
+        alpha = ["A", "B", "C"]
+        for i, row in enumerate(self.board):
+            row_str = " | ".join(row)
+            print(f" {alpha[i]}  {row_str}")
+            if i < len(self.board) - 1:
+                print("   ---+---+---")
 
 
 if __name__ == "__main__":
-    pass
+    game = TicTacToe()
+    game.show_board()
