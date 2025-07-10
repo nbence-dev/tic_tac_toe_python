ef7a51e53aa77829f60a46bcf71f9e29fe560664
Nicholas <nicholasbence12@gmail.com>
2025-07-10 13:21:47 +0200

    Version 1. First working version with colour, determining winners, PC playing against human. Might add multiplayer function (LAN). Change row from numbers to chars.

--- main.py
+++ main.py
@@ -10,19 +10,21 @@ def clear_console():
 # recurring function
 def running():
     # Introduction
-    print("=======================")
-    print("Welcome to Tic Tac Toe!")
-    print("=======================\n")
+    print("\33[92m=======================")
+    print("\33[93mWelcome to Tic Tac Toe!")
+    print("\33[92m=======================\33[0m\n")
 
     # Creates the game
     game = TicTacToe()
 
+    game.show_board()
     # Asks user if they want to replay
     bContinue = True
     # Determines whether a winner has been found
+    winnerChar = ""
     gameOver = False
     # while bContinue:
-    print("You are X:")
+    print("\nYou are X:")
 
     # loops until no one can play anymore
     while not gameOver:
@@ -30,22 +32,51 @@ def running():
         try:
 
             if bPlayer_turn:
-                row = int(input("Choose the row you want to place your x: ")) - 1
-                column = int(input("Choose the column you want to place your x: ")) - 1
+                row = (
+                    int(input("Choose the \33[91mROW\33[0m you want to place your x: "))
+                    - 1
+                )
+                # row = (
+                #     input("Choose the \33[91mROW\33[0m you want to place your x: ")
+                #     .lower()
+                #     .strip()
+                # )
+                column = (
+                    int(
+                        input(
+                            "Choose the \33[91mCOLUMN\33[0m you want to place your x: "
+                        )
+                    )
+                    - 1
+                )
+                print()
                 if row > 2 or row < 0:
-                    print(
-                        "You need to choose between 1-3."
-                    )  # 1 2 3 but in list format it is 0 1 2
+                    print("You need to choose between 1-3.")
+                # if not row in ["A", "B", "C"]:
+                #     print(
+                #         "You need to choose between A-C."
+                #     )  # 1 2 3 but in list format it is 0 1 2
                 elif column > 2 or column < 0:
                     print("You need to choose between 1-3.")
                 else:
-                    game.player_move(row, column)
-                    bPlayer_turn = False
-                    game.pc_move()
+                    bValidMove = game.player_move(row, column)
+                    if bValidMove:
+                        bPlayer_turn = False
+                        winnerDetermined, winnerChar = game.determine_winner("X")
+                        if not winnerDetermined:
+                            game.pc_move()
+                            winnerDetermined, winnerChar = game.determine_winner("O")
+                            if winnerDetermined:
+                                gameOver = True
+                        else:
+                            gameOver = True
         except ValueError:
             print("Only insert numbers, silly!")
 
     # TODO - Determine winner
+    print("\n=================")
+    print(f"The winner is: {winnerChar}")
+    print("=================\n")
 
     # TODO - Ask player if they want to continue
     qContinue = input("Do you want to play again? Type 'y' or 'n': ")
--- tictactoe.py
+++ tictactoe.py
@@ -15,29 +15,137 @@ class TicTacToe:
     #     ['-', '-', '-'],
     #     ['-', '-', '-']
     # ]
-    def player_move(self, row, column):
 
+    def player_move(self, row, column):
         if self.board[row][column] == "X" or self.board[row][column] == "O":
             print("You cannot place your X there.")
+            return False
         elif self.board[row][column] == "-":
             self.board[row][column] = "X"
             self.show_board()
+        return True
 
+    # TODO - Make PC smarter
     def pc_move(self):
-        print("PC is placing his circle...\n")
+        print("\nPC is placing his circle...")
         time.sleep(2)
         while True:
             x = random.randint(0, 2)
             y = random.randint(0, 2)
             if self.board[x][y] == "-":
                 self.board[x][y] = "O"
-                print(f"PC placed O at {chr(65 + x)}{y + 1}")
+                print(f"PC placed O at {chr(65 + x)}{y + 1}\n")
                 self.show_board()
 
                 break
+        # Smarter PC logic (in comments):
+        # 1. Check if PC can win in the next move. If yes, make that move.
+        # 2. Check if player can win in the next move. If yes, block that move.
+        # 3. Otherwise, pick a random empty spot.
 
-    def determine_winner(self):
-        pass
+        # Example implementation steps:
+        # for each empty cell:
+        #     - Temporarily place 'O' in the cell
+        #     - Check if this leads to a win for PC
+        #     - If yes, make this move and return
+        #     - Else, revert the cell
+        # for each empty cell:
+        #     - Temporarily place 'X' in the cell
+        #     - Check if this leads to a win for player
+        #     - If yes, place 'O' there to block and return
+        #     - Else, revert the cell
+        # If no immediate win or block, pick a random empty cell as before.
+
+        # --- CODED SMARTER PC LOGIC BELOW (IN COMMENTS) ---
+        # # 1. Try to win
+        # for i in range(3):
+        #     for j in range(3):
+        #         if self.board[i][j] == "-":
+        #             self.board[i][j] = "O"
+        #             if self.check_win("O"):
+        #                 print(f"PC placed O at {chr(65 + i)}{j + 1}")
+        #                 self.show_board()
+        #                 return
+        #             self.board[i][j] = "-"
+        #
+        # # 2. Block player win
+        # for i in range(3):
+        #     for j in range(3):
+        #         if self.board[i][j] == "-":
+        #             self.board[i][j] = "X"
+        #             if self.check_win("X"):
+        #                 self.board[i][j] = "O"
+        #                 print(f"PC placed O at {chr(65 + i)}{j + 1}")
+        #                 self.show_board()
+        #                 return
+        #             self.board[i][j] = "-"
+        #
+        # # 3. Otherwise, random move
+        # while True:
+        #     x = random.randint(0, 2)
+        #     y = random.randint(0, 2)
+        #     if self.board[x][y] == "-":
+        #         self.board[x][y] = "O"
+        #         print(f"PC placed O at {chr(65 + x)}{y + 1}")
+        #         self.show_board()
+        #         break
+        #
+        # # Helper function needed:
+        # # def check_win(self, symbol):
+        # #     # Check rows, columns, and diagonals for a win for 'symbol'
+        # #     ...
+
+    # To determine the winner in a Tic Tac Toe match, you need to check if either player has succeeded in placing three of their marks in a row. This can happen in three ways:
+    # 1. Horizontally: All three cells in any row contain the same player's symbol.
+    # 2. Vertically: All three cells in any column contain the same player's symbol.
+    # 3. Diagonally: All three cells in either of the two diagonals contain the same player's symbol.
+    # If any of these conditions are met for a player, that player is declared the winner.
+
+    def determine_winner(self, char):
+
+        # TODO - Determine winner horizontally
+        for row in self.board:
+            counter = 0
+            for column in row:
+                if column == char:
+                    counter += 1
+                    if counter == 3:
+                        return True, char
+
+        # TODO - Determine winner vertically
+
+        for _ in range(0, 3):  # self.board[0][1] self.board[1][1] self.board[2][1]
+            counter = 0
+            for value in range(0, 3):
+                if self.board[value][_] == char:
+                    counter += 1
+                    if counter == 3:
+                        return True, char
+
+        # TODO - Determine winner diagonally.
+        l_diagonal = 0
+        for _ in range(0, 3):
+            if self.board[_][_] == char:
+                l_diagonal += 1
+                if l_diagonal == 3:
+                    return True, char
+
+        r_diagonal = 0  # 0,2    1,1    2,0
+        length_of_board = 2
+        for _ in range(0, 3):  # 0,2  1,1    2,0
+            if self.board[_][length_of_board] == char:
+                r_diagonal += 1
+                length_of_board -= 1
+                if r_diagonal == 3:
+                    return True, char
+        # TODO - Determine draw
+        if all(cell != "-" for row in self.board for cell in row):
+            return True, "draw"
+
+        return False, None
+
+        # TODO - Determine winner vertically
+        # TODO - Determine winner diagonally
 
         #    1   2   3
         # A  X | O |
